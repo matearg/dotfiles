823aea2a5c1279a595b812b801128df4a63a8915
Mateo Arcidiacono <mateoarcidiacono@gmail.com>
2022-07-22 19:37:36 -0300

    Added gruvbox theme

--- qtile/config.py
+++ qtile/config.py
@@ -226,10 +226,10 @@ for i in groups:
 
 
 def init_layout_theme():
-    return {"margin":8,
-            "border_width":2,
-            "border_focus": "#5e81ac",
-            "border_normal": "#4c566a"
+    return {"margin": 8,
+            "border_width": 2,
+            "border_focus": "#83a598",
+            "border_normal": "#3c3836"
             }
 
 layout_theme = init_layout_theme()
@@ -246,21 +246,22 @@ layouts = [
 ]
 
 # COLORS FOR THE BAR
-#Theme name : ArcoLinux Nord
+#Theme name : ArcoLinux Gruvbox
 def init_colors():
-    return [["#3B4252", "#3B4252"], # color 0
-            ["#2e3440", "#2e3440"], # color 1
-            ["#A3BE8C", "#A3BE8C"], # color 2
-            ["#EBCB8B", "#EBCB8B"], # color 3
-            ["#81A1C1", "#81A1C1"], # color 4
-            ["#D8DEE9", "#D8DEE9"], # color 5
-            ["#88C0D0", "#88C0D0"], # color 6
-            ["#E5E9F0", "#E5E9F0"], # color 7
-            ["#4C566A", "#4C566A"], # color 8
-            ["#BF616A", "#BF616A"]] # color 9
+    return [["#282828", "#282828"], # color 0
+            ["#3c3836", "#3c3836"], # color 1
+            ["#98971a", "#98971a"], # color 2
+            ["#d79921", "#d79921"], # color 3
+            ["#cc241d", "#cc241d"], # color 4
+            ["#ebdbb2", "#ebdbb2"], # color 5
+            ["#83a598", "#83a598"], # color 6
+            ["#fbf1c7", "#fbf1c7"], # color 7
+            ["#1d2021", "#1d2021"], # color 8
+            ["#458588", "#458588"]] # color 9
 
 
 colors = init_colors()
+
 def init_widgets_defaults():
     return dict(font="Noto Sans",
                 fontsize = 12,
